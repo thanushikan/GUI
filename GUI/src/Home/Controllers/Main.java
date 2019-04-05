4691e88bc76f173146a6cd2b0820af77dcfa1073
thanushikan <48682207+thanushikan@users.noreply.github.com>
2019-04-05 15:02:16 +0100

    Add files via upload

--- GUI/src/Home/Controllers/Main.java
+++ GUI/src/Home/Controllers/Main.java
@@ -1,4 +1,37 @@
 package Home.Controllers;
 
-public class Main {
+import java.awt.*;
+import com.jfoenix.controls.JFXButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.Pane;
+
+
+public class Main implements Initializable {
+    @FXML
+    private Pane pnl_all,pnl_Account,pnl_EditAccount,pnl_ViewCF;
+
+    @FXML
+    private JFXButton  label;
+
+
+    @FXML
+    private void handleButtonAction(ActionEvent event){
+        System.out.println("Ypu Clikkkked");
+        label.setText("Hello World");
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+
+
+    }
+
 }
+
